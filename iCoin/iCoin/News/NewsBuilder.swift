3d77d61082f30b65a5a4ecd75f450301d4bc3915
hrKim0303 <85341050+hrKim0303@users.noreply.github.com>
2022-06-05 05:01:43 +0900

    feat add news

---
+++ iCoin/iCoin/News/NewsBuilder.swift
@@ -0,0 +1,39 @@
+//
+//  NewsBuilder.swift
+//  iCoin
+//
+//  Created by 김윤석 on 2022/06/05.
+//
+
+import ModernRIBs
+
+protocol NewsDependency: Dependency {
+    // TODO: Declare the set of dependencies required by this RIB, but cannot be
+    // created by this RIB.
+}
+
+final class NewsComponent: Component<NewsDependency> {
+
+    // TODO: Declare 'fileprivate' dependencies that are only used by this RIB.
+}
+
+// MARK: - Builder
+
+protocol NewsBuildable: Buildable {
+    func build(withListener listener: NewsListener) -> NewsRouting
+}
+
+final class NewsBuilder: Builder<NewsDependency>, NewsBuildable {
+
+    override init(dependency: NewsDependency) {
+        super.init(dependency: dependency)
+    }
+
+    func build(withListener listener: NewsListener) -> NewsRouting {
+        let component = NewsComponent(dependency: dependency)
+        let viewController = NewsViewController()
+        let interactor = NewsInteractor(presenter: viewController)
+        interactor.listener = listener
+        return NewsRouter(interactor: interactor, viewController: viewController)
+    }
+}
