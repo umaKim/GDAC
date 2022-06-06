edbb8768f300dab9511e26ad9f190cbca19c272c
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-06-07 01:11:57 +0900

    chore: add newsVC

--- iCoin/iCoin/Core/News/NewsViewController.swift
+++ iCoin/iCoin/Core/News/NewsViewController.swift
@@ -14,7 +14,79 @@ protocol NewsPresentableListener: AnyObject {
     // interactor class.
 }
 
-final class NewsViewController: UIViewController, NewsPresentable, NewsViewControllable {
+final class NewsViewController: UIViewController, NewsPresentable, NewsViewControllable, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .blue
+        
+        return cell
+    }
+    
 
     weak var listener: NewsPresentableListener?
+    
+    private(set) var tableView: UITableView = {
+       let tv = UITableView()
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tv
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        tableView.dataSource = self
+        
+        setTV()
+    }
+    
+    private func setTV() {
+        view.addSubviews(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor)
+        ])
+    }
 }
+//
+//extension NewsViewController: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+////        viewModel.stories.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: NewsStoryTableViewCell.identfier, for: indexPath) as? NewsStoryTableViewCell else { return UITableViewCell()}
+//        cell.configure(with: .init(model: viewModel.stories[indexPath.row]))
+//        return cell
+//    }
+//}
+//
+//extension NewsViewController: UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        guard let header = tableView.dequeueReusableHeaderFooterView(
+//            withIdentifier: NewsHeaderView.identifier
+//        ) as? NewsHeaderView else { return nil }
+//        header.configure(with: .init( title: "Top News", shouldShowAddButton: false))
+//
+//        return header
+//    }
+//
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return NewsStoryTableViewCell.preferredHeight
+//    }
+//
+//    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//        return NewsHeaderView.preferredHeight
+//    }
+//
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        viewModel.didSelectNews(at: indexPath)
+//    }
+//}
