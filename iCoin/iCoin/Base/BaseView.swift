29fd4a0c80e24e1fcd823a22114f6c8d41dbeaf4
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-06-07 01:10:55 +0900

    chore: add baseView

---
+++ iCoin/iCoin/Base/BaseView.swift
@@ -0,0 +1,14 @@
+//
+//  BaseView.swift
+//  iCoin
+//
+//  Created by 김윤석 on 2022/06/05.
+//
+
+import UIKit.UIView
+import Combine
+import CombineCocoa
+
+class BaseView: UIView {
+    var cancellables = Set<AnyCancellable>()
+}
