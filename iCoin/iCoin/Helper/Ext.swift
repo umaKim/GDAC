f2f5ceccb5b1d692d78e717c7ecb742a42d2e933
hrKim0303 <85341050+hrKim0303@users.noreply.github.com>
2022-06-06 23:13:08 +0900

    chore: move extension file to helper file

---
+++ iCoin/iCoin/Helper/Ext.swift
@@ -0,0 +1,55 @@
+//
+//  Ext.swift
+//  iCoin
+//
+//  Created by 김윤석 on 2022/06/05.
+//
+
+import UIKit
+
+// MARK: - Add Subview
+
+extension UIView {
+    /// Adds multiple subviews
+    /// - Parameter views: Collection of subviews
+    func addSubviews(_ views: UIView...) {
+        views.forEach {
+            addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+}
+
+// MARK: - Framing
+
+extension UIView {
+    /// Width of view
+    var width: CGFloat {
+        frame.size.width
+    }
+
+    /// Height of view
+    var height: CGFloat {
+        frame.size.height
+    }
+
+    /// Left edge of view
+    var left: CGFloat {
+        frame.origin.x
+    }
+
+    /// Right edge of view
+    var right: CGFloat {
+        left + width
+    }
+
+    /// Top edge of view
+    var top: CGFloat {
+        frame.origin.y
+    }
+
+    /// Bottom edge of view
+    var bottom: CGFloat {
+        top + height
+    }
+}
