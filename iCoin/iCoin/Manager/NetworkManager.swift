5d6150fce3e73f97f8ed77ab944d81cad8fd3c52
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-06-07 23:27:58 +0900

    feat: add networkManager and websocket

---
+++ iCoin/iCoin/Manager/NetworkManager.swift
@@ -0,0 +1,68 @@
+//
+//  NetworkManager.swift
+//  iCoin
+//
+//  Created by 김윤석 on 2022/06/07.
+//
+import Combine
+import Foundation
+
+//MARK: - News API Key
+//"https://min-api.cryptocompare.com/documentation"
+
+protocol NetworkProtocol {
+    func fetchNews(of symbol: String) -> AnyPublisher<NewsDataResponse, Error>
+}
+
+final class NetworkManager: NetworkProtocol, UrlConfigurable {
+    fileprivate enum NewsConstants {
+        static let apiKey = "508858945962b1d801891796a6d67f8076873f9996b13b48e5eb63030be21ec4"
+        static let newsBaseUrl = "https://min-api.cryptocompare.com/data/news/feeds/?api_key="
+    }
+    
+    /// API Errors
+    private enum APIError: Error {
+        case noDataReturned
+        case invalidUrl
+    }
+    
+    ///Perform News api call
+    ///return AnyPublisher containing NewsDataResponse
+    func fetchNews(of symbol: String = "") -> AnyPublisher<NewsDataResponse, Error> {
+        let url = url(for: NewsConstants.newsBaseUrl, with: ["api_key": NewsConstants.apiKey])
+        return request(url: url, expecting: NewsDataResponse.self)
+    }
+    
+    /// Perform api call
+    /// - Parameters:
+    ///   - url: URL to hit
+    ///   - expecting: Type we expect to decode data to
+    ///   - completion: Result callback
+    private func request<T: Codable>(url: URL?, expecting: T.Type) -> AnyPublisher<T, Error> {
+        Future { promise in
+            guard let url = url else { return
+                promise(.failure( APIError.invalidUrl))}
+            
+            URLSession.shared.dataTask(with: url) { data, _, error in
+                guard let data = data, error == nil else {
+                    if let error = error {
+                        promise(.failure(error))
+                    } else {
+                        promise(.failure(APIError.noDataReturned))
+                    }
+                    return
+                }
+                
+                do {
+                    let result = try JSONDecoder().decode(expecting, from: data)
+                    promise(.success(result))
+                }
+                catch {
+                    promise(.failure(error))
+                }
+            }
+            .resume()
+        }
+        .eraseToAnyPublisher()
+    }
+}
---
+++ iCoin/iCoin/Manager/WebsocketManager.swift
@@ -0,0 +1,97 @@
+//
+//  WebsocketManager.swift
+//  iCoin
+//
+//  Created by 김윤석 on 2022/06/07.
+//
+import Combine
+import Foundation
+
+protocol WebSocketProtocol {
+    var dataPublisher: PassthroughSubject<[Datum], Never> { get set }
+    func webSocket (symbols: [String])
+    func receiveMessage()
+}
+
+final class WebSocketManager : WebSocketProtocol, UrlConfigurable {
+    
+    fileprivate enum CryptoConstants {
+        static let apiKey = "c3c6me2ad3iefuuilms0"
+        static let baseUrl = "wss://ws.finnhub.io"
+    }
+    
+    private var webSocketTask : URLSessionWebSocketTask?
+    
+    var dataPublisher = PassthroughSubject<[Datum], Never>()
+    
+    init() {
+        guard
+            let url = url(for: CryptoConstants.baseUrl, with: ["token" : CryptoConstants.apiKey])
+        else {
+            print("unable to url")
+            return }
+        
+        webSocketTask = URLSession(configuration: .default).webSocketTask(with: url)
+    }
+    
+    /// Setup Websocket with specific input values
+    func webSocket(symbols: [String]) {
+        DispatchQueue.global().async {
+            let set = Set(symbols)
+            for symbol in set {
+                let symbolForFinHub = "BINANCE:\(symbol)USDT"
+                let message = URLSessionWebSocketTask.Message.string("{\"type\":\"subscribe\",\"symbol\":\"\(symbolForFinHub)\"}")
+                
+                self.webSocketTask?.send(message) { error in
+                    if let error = error {
+                        print("WebSocket couldn’t send message because: \(error)")
+                    }
+                }
+            }
+            self.webSocketTask?.resume()
+            self.ping()
+        }
+    }
+    
+    private func ping() {
+        DispatchQueue.global().async(qos: .utility) {
+            self.webSocketTask?.sendPing { error in
+                if let error = error {
+                    print("Error when sending PING \(error)")
+                } else {
+                    print("Web Socket connection is alive")
+                    DispatchQueue.global().asyncAfter(deadline: .now() + 5) {
+                        self.ping()
+                    }
+                }
+            }
+        }
+    }
+    
+    /// Perform Websocket
+    func receiveMessage() {
+        DispatchQueue.global().async {
+            self.webSocketTask?.receive { [weak self] result in
+                guard let self = self else {return}
+                switch result {
+                case .failure(let error):
+                    print("Error in receiving message: \(error)")
+                case .success(let message):
+                    switch message {
+                    case .string(let text):
+                        if let data: Data = text.data(using: .utf8) {
+                            if let tickData = try? WebSocketData.decode(from: data)?.data {
+                                self.dataPublisher.send(tickData)
+                            }
+                        }
+                    case .data(let data):
+                        print("Received data: \(data)")
+                    @unknown default:
+                        fatalError()
+                    }
+                    self.receiveMessage()
+                }
+            }
+        }
+    }
+}
