d1c911f465c9478fbe7786a686a095aa9d8a0378
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2022-06-12 19:38:09 +0900

    chore: add custom navigation back button

---
+++ iCoin/iCoin/Helper/UIViewController+Utils.swift
@@ -0,0 +1,35 @@
+//
+//  UIViewController+Utils.swift
+//  iCoin
+//
+//  Created by 김윤석 on 2022/06/12.
+//
+
+import UIKit
+
+enum DissmissButtonType {
+    case back, close
+    
+    var iconSystemName: String {
+        switch self {
+        case .close:
+            return "xmark"
+        case .back:
+            return "chevron.backward"
+        }
+    }
+}
+
+extension UIViewController {
+    func setupNavigationItem(with buttonType: DissmissButtonType, tintColor: UIColor = .tintColor, target: Any?, action: Selector?) {
+        let bt = UIBarButtonItem(
+            image: UIImage(
+                systemName: buttonType.iconSystemName,
+                withConfiguration: UIImage.SymbolConfiguration(pointSize: 18, weight: .semibold)),
+            style: .plain,
+            target: target,
+            action: action)
+        bt.tintColor = tintColor
+        navigationItem.leftBarButtonItem = bt
+    }
+}
