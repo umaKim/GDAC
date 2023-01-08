4fe8b35be8a6ef31079027792dfbe9e928dcae94
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2023-01-09 00:53:07 +0900

    chore: configure firebase

--- iCoin/iCoin/AppDelegate/AppDelegate.swift
+++ iCoin/iCoin/AppDelegate/AppDelegate.swift
@@ -4,13 +4,14 @@
 //
 //  Created by 김윤석 on 2022/06/05.
 //
-
+import Firebase
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
