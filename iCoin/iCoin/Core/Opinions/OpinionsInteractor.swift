3f1dce68c89c2bd1c725fc3be3bfc53d0f119bf1
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2023-01-09 01:13:06 +0900

    feat: set Opinions page

--- iCoin/iCoin/Core/Opinions/OpinionsInteractor.swift
+++ iCoin/iCoin/Core/Opinions/OpinionsInteractor.swift
@@ -4,7 +4,9 @@
 //
 //  Created by 김윤석 on 2022/06/05.
 //
-
+import Combine
+import FirebaseDatabase
+import UIKit.UITableView
 import ModernRIBs
 
 protocol OpinionsRouting: ViewableRouting {
@@ -14,6 +16,8 @@ protocol OpinionsRouting: ViewableRouting {
 protocol OpinionsPresentable: Presentable {
     var listener: OpinionsPresentableListener? { get set }
     // TODO: Declare methods the interactor can invoke the presenter to present data.
+    func opinions(with data: [PostContent])
+    func reloadData(with data: [PostContent], animation: UITableView.RowAnimation)
 }
 
 protocol OpinionsListener: AnyObject {
@@ -31,9 +35,18 @@ final class OpinionsInteractor: PresentableInteractor<OpinionsPresentable>, Opin
     
     private let dependency: OpinionInteractorDependency
 
+    private(set) var opinions: [PostContent] =  []
+    
+    private var cancellalbles: Set<AnyCancellable>
+    
     // TODO: Add additional dependencies to constructor. Do not perform any logic
     // in constructor.
-    override init(presenter: OpinionsPresentable) {
+    init(
+        presenter: OpinionsPresentable,
+        dependency: OpinionInteractorDependency
+    ) {
+        self.dependency = dependency
+        self.cancellalbles = .init()
         super.init(presenter: presenter)
         presenter.listener = self
     }
@@ -41,6 +54,17 @@ final class OpinionsInteractor: PresentableInteractor<OpinionsPresentable>, Opin
     override func didBecomeActive() {
         super.didBecomeActive()
         // TODO: Implement business logic here.
+        dependency
+            .firebaseRepository
+            .fetchOpinions()
+            .receive(on: RunLoop.main)
+            .sink {[weak self] content in
+                guard let self = self else { return }
+                self.opinions.append(content)
+                self.opinions.sort(by: {$0.date > $1.date})
+                self.presenter.opinions(with: self.opinions)
+            }
+            .store(in: &cancellalbles)
     }
 
     override func willResignActive() {
--- iCoin/iCoin/Core/Opinions/OpinionsViewController.swift
+++ iCoin/iCoin/Core/Opinions/OpinionsViewController.swift
@@ -14,7 +14,73 @@ protocol OpinionsPresentableListener: AnyObject {
     // interactor class.
 }
 
-final class OpinionsViewController: UIViewController, OpinionsPresentable, OpinionsViewControllable {
+final class OpinionsViewController: UIViewController, OpinionsViewControllable {
+   
+    private var models: [PostContent] = []
 
+    private typealias DataSource = UITableViewDiffableDataSource<Section, PostContent>
+    private typealias Snapshot = NSDiffableDataSourceSnapshot<Section, PostContent>
+    enum Section { case main }
+    
     weak var listener: OpinionsPresentableListener?
+    
+    private var dataSource: DataSource?
+    
+    private let contentView = OpinionsView()
+    
+    override func loadView() {
+        super.loadView()
+        view = contentView
+        
+        configureTableViewDataSource()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        reloadData(with: self.models, animation: .fade)
+    }
+}
+
+// MARK: - OpinionsPresentable
+extension OpinionsViewController: OpinionsPresentable {
+    func opinions(with data: [PostContent]) {
+        self.models = data
+    }
+    
+    func reloadData(
+        with data: [PostContent],
+        animation: UITableView.RowAnimation
+    ) {
+        var snapshot = Snapshot()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(data)
+        dataSource?.apply(snapshot, animatingDifferences: true)
+        dataSource?.defaultRowAnimation = animation
+    }
+}
+
+//MARK: - UITableViewDataSource
+extension OpinionsViewController {
+    private func configureTableViewDataSource() {
+        contentView.tableView.delegate = self
+        dataSource = .init(
+            tableView: contentView.tableView,
+            cellProvider: { (tableView, indexPath, item) -> UITableViewCell? in
+                guard
+                    let cell = tableView.dequeueReusableCell(
+                        withIdentifier: CommentTableViewCell.identifier,
+                        for: indexPath
+                    ) as? CommentTableViewCell
+                else { return nil }
+                cell.configure(with: item)
+                return cell
+            })
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension OpinionsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
