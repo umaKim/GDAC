e213d03406187a4abe5592af2760823cb7638f67
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2023-01-15 23:59:55 +0900

    chore: set SymbolsConstantRepositoryImp

--- iCoin/iCoin/Repository/SymbolsRepository.swift
+++ iCoin/iCoin/Repository/SymbolsRepository.swift
@@ -21,6 +21,38 @@ final class SymbolsRepositoryImp: SymbolsRepository {
     
     func fetchSymbols() -> AnyPublisher<[SymbolResult], Error> {
         return network.fetchSymbols()
+final class SymbolsConstantRepositoryImp: SymbolsRepository {
+    func fetchSymbols() -> AnyPublisher<[SymbolResult], Error> {
+        Future { promise in
+            promise(.success(StaticSymbols.symbols))
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    enum StaticSymbols {
+        static let symbols: [SymbolResult] = [
+            .init(description: "Binance BTCUSDT", displaySymbol: "BTC/USDT", symbol: "BTCUSDT"),
+            .init(description: "Binance ETHUSDT", displaySymbol: "ETH/USDT", symbol: "ETHUSDT"),
+    //        .init(description: "Binance USDCUSDT", displaySymbol: "USDC/USDT", symbol: "USDCUSDT"),
+            .init(description: "Binance BNBUSDT", displaySymbol: "BNB/USDT", symbol: "BNBUSDT"),
+            .init(description: "Binance XRPUSDT", displaySymbol: "XRP/USDT", symbol: "XRPUSDT"),
+            .init(description: "Binance BUSDUSDT", displaySymbol: "BUSD/USDT", symbol: "BUSDUSDT"),
+            .init(description: "Binance ADAUSDT", displaySymbol: "ADA/USDT", symbol: "ADAUSDT"),
+            .init(description: "Binance MATICUSDT", displaySymbol: "MATIC/USDT", symbol: "MATICUSDT"),
+    //        .init(description: "Binance DAIUSDT", displaySymbol: "DAI/USDT", symbol: "DAIUSDT"),
+            .init(description: "Binance LTCUSDT", displaySymbol: "LTC/USDT", symbol: "LTCUSDT"),
+            .init(description: "Binance DOTUSDT", displaySymbol: "DOT/USDT", symbol: "DOTUSDT"),
+            .init(description: "Binance SOLUSDT", displaySymbol: "SOL/USDT", symbol: "SOLUSDT"),
+            .init(description: "Binance TRXUSDT", displaySymbol: "TRX/USDT", symbol: "TRXUSDT"),
+            .init(description: "Binance UNIUSDT", displaySymbol: "UNI/USDT", symbol: "UNIUSDT"),
+            .init(description: "Binance AVAXUSDT", displaySymbol: "AVAX/USDT", symbol: "AVAXUSDT"),
+            .init(description: "Binance LINKUSDT", displaySymbol: "LINK/USDT", symbol: "LINKUSDT"),
+            .init(description: "Binance ATOMUSDT", displaySymbol: "ATOM/USDT", symbol: "ATOMUSDT"),
+            .init(description: "Binance XMRUSDT", displaySymbol: "XMR/USDT", symbol: "XMRUSDT"),
+            .init(description: "Binance ETCUSDT", displaySymbol: "ETC/USDT", symbol: "ETCUSDT"),
+        ]
+    }
+}
     }
 }
 
