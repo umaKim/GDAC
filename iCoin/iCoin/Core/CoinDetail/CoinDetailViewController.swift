521e30c8b54238b91b2cae7684b3735ec519ec02
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2023-01-17 14:09:23 +0900

    feat: set up ui

--- iCoin/iCoin/Core/CoinDetail/CoinDetailViewController.swift
+++ iCoin/iCoin/Core/CoinDetail/CoinDetailViewController.swift
@@ -4,9 +4,10 @@
 //
 //  Created by 김윤석 on 2023/01/07.
 //
-
+import Combine
 import ModernRIBs
 import UIKit
+import SwiftUI
 
 protocol CoinDetailPresentableListener: AnyObject {
     // TODO: Declare properties and methods that the view controller can invoke to perform
@@ -17,10 +18,9 @@ protocol CoinDetailPresentableListener: AnyObject {
 }
 
 final class CoinDetailViewController: UIViewController, CoinDetailPresentable, CoinDetailViewControllable {
-
+    
     weak var listener: CoinDetailPresentableListener?
     
-    var scrollView = UIScrollView()
     private lazy var favoriteButton = UIBarButtonItem(
         image: .init(systemName: ""),
         style: .done,
@@ -28,10 +28,31 @@ final class CoinDetailViewController: UIViewController, CoinDetailPresentable, C
         action: #selector(favoriteButtonDidTap)
     )
     
+    var contentScrollView = UIScrollView()
+    
+    private var cancellables: Set<AnyCancellable>
+    
+    init() {
+        self.cancellables = .init()
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigationItem(
+            with: .back,
+            tintColor: .label,
+            target: self,
+            action: #selector(backButtonDidTap)
+        )
         
-        setupNavigationItem(with: .back, tintColor: .label, target: self, action: #selector(backButtonDidTap))
+        setupUI()
+    }
+    
     @objc
     private func favoriteButtonDidTap() {
         listener?.didTapFavoriteButton()
@@ -43,8 +64,14 @@ final class CoinDetailViewController: UIViewController, CoinDetailPresentable, C
     }
     
     private var contentView = UIView()
+    
+    private lazy var label: UILabel = UILabel()
+    
     func update(symbol: SymbolResult) {
-        label.text = symbol.symbol
+        label.numberOfLines = 5
+        label.text = "\(symbol.symbol)\n\(symbol.displaySymbol)\n\(symbol.description)"
+    }
+    
     func doesSymbolInPersistance(_ exist: Bool) {
         favoriteButton.image = .init(systemName: exist ? "star.fill" : "star")
     }
@@ -52,10 +79,37 @@ final class CoinDetailViewController: UIViewController, CoinDetailPresentable, C
 
 extension CoinDetailViewController {
     private func setupUI() {
-        view.addSubview(scrollView)
-        scrollView.addSubview(contentView)
-        contentView.addSubview(UIView())
+        
+        let chartView = UIHostingController(rootView: ChartView())
+        guard let chartView = chartView.view else { return }
+        
         favoriteButton.tintColor = .gdacBlue
         navigationItem.rightBarButtonItems = [favoriteButton]
+        
+        view.backgroundColor = .brown
+        view.addSubviews(contentScrollView)
+        contentScrollView.addSubviews(contentView)
+        contentView.addSubviews(label, chartView)
+        
+        label.textColor = .red
+        
+        NSLayoutConstraint.activate([
+            contentScrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            contentScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentScrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            contentScrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            contentView.topAnchor.constraint(equalTo: contentScrollView.safeAreaLayoutGuide.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: contentScrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: contentScrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: contentScrollView.safeAreaLayoutGuide.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: contentScrollView.widthAnchor),
+            
+            label.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            
+            chartView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            chartView.topAnchor.constraint(equalTo: label.bottomAnchor)
+        ])
     }
 }
