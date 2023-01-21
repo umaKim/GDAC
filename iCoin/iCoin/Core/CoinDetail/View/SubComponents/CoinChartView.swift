c5bdd2bfc3de5ab30df484f1dc00c48c6965fdc4
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2023-01-21 12:47:29 +0900

    feat: add segment selector

--- iCoin/iCoin/Core/CoinDetail/View/SubComponents/CoinChartView.swift
+++ iCoin/iCoin/Core/CoinDetail/View/SubComponents/CoinChartView.swift
@@ -5,15 +5,33 @@
 //  Created by 김윤석 on 2023/01/17.
 //
 
+import Combine
 import SwiftUI
 import UIKit
 
-final class CoinChartView: UIView {
+class CoinChartViewData: ObservableObject {
+    @Published var data = [Double]()
+}
+
+enum CoinChartViewAction {
+    case selectedDays(String)
+}
+
+final class CoinChartView: BaseView {
     private var chartData = CoinChartViewData()
+    private lazy var segmentedControl: UISegmentedControl = {
+       let control = UISegmentedControl(items: listOfDays)
+        control.selectedSegmentIndex = 0
+       return control
+     }()
+    private let listOfDays = ["1", "7", "14", "30", "365"]
+    
+    private(set) lazy var actionPublisher = actionSubject.eraseToAnyPublisher()
+    private let actionSubject = PassthroughSubject<CoinChartViewAction, Never>()
     
     init() {
         super.init(frame: .zero)
-        
+        bind()
         setupLayout()
     }
     
@@ -24,25 +42,40 @@ final class CoinChartView: UIView {
     public func configure(with data: [Double] ) {
         chartData.data = data
     }
-    
+}
+
+// MARK: - Bind
+extension CoinChartView {
+    private func bind() {
+        segmentedControl
+            .selectedSegmentIndexPublisher
+            .sink {[weak self] index in
+                if index <= -1 { return }
+                guard let self = self else { return }
+                self.actionSubject.send(.selectedDays(self.listOfDays[index]))
+            }
+            .store(in: &cancellables)
+    }
+}
+
+// MARK: - Set up UI
+extension CoinChartView {
     private func setupLayout() {
         let chartView = ChartView()
         let chartViewControllerView = UIHostingController(rootView: chartView.environmentObject(chartData)).view!
-        
-        addSubviews(chartViewControllerView)
-        
+        addSubviews(chartViewControllerView, segmentedControl)
         NSLayoutConstraint.activate([
-            chartViewControllerView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            chartViewControllerView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            chartViewControllerView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            chartViewControllerView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            chartViewControllerView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
             chartViewControllerView.topAnchor.constraint(equalTo: topAnchor),
+            
+            segmentedControl.topAnchor.constraint(equalTo: chartViewControllerView.bottomAnchor, constant: 8),
+            segmentedControl.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            segmentedControl.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            segmentedControl.bottomAnchor.constraint(equalTo: bottomAnchor),
             
             heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.width),
             widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width)
         ])
     }
-}
-
-class CoinChartViewData: ObservableObject {
-    @Published var data = [Double]()
 }
