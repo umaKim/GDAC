06a86be3fd00657ba0e694c9b39950e0829d422b
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2023-01-21 11:54:02 +0900

    chore: delete unnecessary codes

--- iCoin/iCoin/Core/Portfolio/PortfolioViewController.swift
+++ iCoin/iCoin/Core/Portfolio/PortfolioViewController.swift
@@ -21,8 +21,6 @@ final class PortfolioViewController: UIViewController, PortfolioPresentable, Por
     
     weak var listener: PortfolioPresentableListener?
     
-    private let contentView = PortfolioView()
-    
     init() {
         super.init(nibName: nil, bundle: nil)
         setupTabBarItem()
@@ -30,12 +28,6 @@ final class PortfolioViewController: UIViewController, PortfolioPresentable, Por
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func loadView() {
-        super.loadView()
-        
-        view = contentView
     }
     
     override func viewDidLoad() {
