f61deccb4db95f2f6382d437808e0ab27ab566b0
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2023-02-25 20:51:53 +0900

    chore: move file to Views folder

---
+++ iCoin/iCoin/Views/TableViewDiffableDataSource.swift
@@ -0,0 +1,10 @@
+//
+//  TableViewDiffableDataSource.swift
+//  iCoin
+//
+//  Created by 김윤석 on 2022/06/06.
+//
+
+import UIKit
+
+final class TableViewDiffableDataSource: UITableViewDiffableDataSource<Section, WatchlistItemModel> { }
