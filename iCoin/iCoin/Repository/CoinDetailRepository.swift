ad70cf932749ec05f38947cadd35df1e662a9c9c
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2023-02-27 20:10:43 +0900

    chore: apply websocket

--- iCoin/iCoin/Repository/CoinDetailRepository.swift
+++ iCoin/iCoin/Repository/CoinDetailRepository.swift
@@ -8,23 +8,31 @@ import Combine
 import Foundation
 
 protocol CoinDetailRepository {
+    
+    /// persistance
     func contains(_ symbol: CoinCapAsset) -> Bool
     func save(_ symbol: CoinCapAsset)
     func remove(_ symbol: CoinCapAsset)
     
-    func connect()
+    /// ws
+    var dataPublisher: AnyPublisher<[Datum], Never> { get }
+    func set(symbols: [String])
     func disconnect()
-    func fetch(symbol: String)
+    func connect()
     
+    /// networkManager
     func fetchMetaData(of symbol: String) -> AnyPublisher<CoinCapDetail, Error>
 }
 
 typealias CoinDetailNetworkable = CoinChartDataNetworkable & CoinCapMetaNetworkable
 
-struct CoinDetailRepositoryImp: CoinDetailRepository {
+class CoinDetailRepositoryImp: CoinDetailRepository {
     private let persistance: PersistanceService
     private let websocket: WebSocketProtocol
     private let network: CoinDetailNetworkable
+    
+    lazy var dataPublisher: AnyPublisher<[Datum], Never> = dataSubject.eraseToAnyPublisher()
+    private var dataSubject = PassthroughSubject<[Datum], Never>()
     
     init(
         _ persistance: PersistanceService,
@@ -35,10 +43,13 @@ struct CoinDetailRepositoryImp: CoinDetailRepository {
         self.websocket = websocket
         self.network = network
     }
+    
+    private var symbols = [String]()
 }
 
 // MARK: - Persistance
 extension CoinDetailRepositoryImp {
+    
     func contains(_ symbol: CoinCapAsset) -> Bool {
         persistance.watchlistContains(symbol: symbol)
     }
@@ -53,17 +64,42 @@ extension CoinDetailRepositoryImp {
 }
 
 // MARK: - Websocket
-extension CoinDetailRepositoryImp {
+extension CoinDetailRepositoryImp: StarScreamWebSocketDelegate {
+    
+    func set(symbols: [String]) {
+        self.symbols = symbols
+    }
+    
     func connect() {
-        websocket.connect()
+        websocket.register(delegate: self)
+        websocket.connect(to: FinnHubSocket())
     }
     
     func disconnect() {
         websocket.disconnect()
     }
     
-    func fetch(symbol: String) {
-        websocket.set(symbols: [symbol])
+    //MARK: - StarScreamWebSocketDelegate
+    func cancelled() {
+        connected()
+    }
+    
+    func connected() {
+        DispatchQueue.global().async {[weak self] in
+            self?.symbols.forEach({ symbol in
+                let symbolForFinHub = "BINANCE:\(symbol)USDT"
+                let message = "{\"type\":\"subscribe\",\"symbol\":\"\(symbolForFinHub)\"}"
+                self?.websocket.write(message)
+            })
+        }
+    }
+    
+    func didReceive(_ text: String) {
+        if let data: Data = text.data(using: .utf8) {
+            if let tickData = try? WebSocketData.decode(from: data)?.data {
+                self.dataSubject.send(tickData)
+            }
+        }
     }
 }
 
